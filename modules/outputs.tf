8cb20116e53bb29a6d5c3422762c720c43a8bb44
Guy Barros <guy@hashicorp.com>
2018-11-28 14:34:05 +0000

    adding revoke_accesor to nomad vault policy

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,5 +1,15 @@
-/*
+
 output "consul_servers" {
-  value = ["${azurerm_public_ip.server-pip.*.fqdn}"]
+    value = "${formatlist("http://%s:8500/", azurerm_public_ip.server-pip.*.fqdn,)}"
 }
+
+/**
+output "nomad_workers_consul_ui" {
+  value = "${formatlist("http://%s:8500/", aws_instance.workers.*.public_ip,)}"
+}
+
+output "nomad_workers_ui" {
+  value = "${formatlist("http://%s:3000/", aws_instance.workers.*.public_ip)}"
+}
+
 */
