542a6bab2fdb887f12206a36bac1a99d3f1ef54d
Guy Barros <guy@hashicorp.com>
2020-04-29 12:07:53 -0300

    commit latest code, issues with tuple persist

--- modules/azure.tf
+++ modules/azure.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  subscription_id        = var.subscription_id
+ subscription_id        = var.subscription_id
  tenant_id              = var.tenant_id
  client_id           = var.client_id
  client_secret       = var.client_secret
